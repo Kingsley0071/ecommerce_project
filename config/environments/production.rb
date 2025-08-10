4d5cae83484e6decafd6e588f4be4eccf77268ce
Kingsley Ogueri <kogueri@rrc.ca>
2025-08-09 21:10:09 -0500

    Edited /config/environments/production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
+  
+  # Set up active storage
+  config.active_storage.service = :amazon # or :google
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [

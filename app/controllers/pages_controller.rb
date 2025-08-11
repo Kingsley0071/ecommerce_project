9b807333faffba849f4781128ddc1f833badf03d
Kingsley Ogueri <kogueri@rrc.ca>
2025-08-10 19:09:04 -0500

    Updated the routes.rb file and the app/controllers/pages_controller.rb file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
+  def about
+    @breadcrumbs = [
+      { name: "Home", path: root_path },
+      { name: "About" }
+    ]
+  end
+
   def show
     @page = Page.find_by(title: params[:id].capitalize)
     @breadcrumbs = [
       { name: "Home", path: root_path },
-      { name: @page.title, path: page_path(params[:id]) }
+      { name: @page&.title || params[:id].capitalize, path: page_path(params[:id]) }
     ]
     render :show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,32 +6,20 @@ Rails.application.routes.draw do
   get "carts/show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/about", to: "pages#show", defaults: { id: "about" }
+  # Add a direct route for the about page
+  get "/about", to: "pages#about"
   get "/contact", to: "pages#show", defaults: { id: "contact" }
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Create a Stripe Checkout Route and Controller Action
   post "create_checkout_session", to: "orders#create_checkout_session"
   post "cart/create_checkout_session", to: "carts#create_checkout_session", as: :cart_create_checkout_session
-
-  # config/routes.rb
   post "stripe/webhook", to: "stripe_webhooks#create"
 
-  # Defines the root path route ("/")
   root "products#index"
 
-  # Enable full RESTful routes for products (including edit, update, destroy, new, create)
   resources :products
-
   resources :categories, only: [ :index, :show ]
   resource :cart, only: [ :show ]
   resources :pages, only: [ :show ]
